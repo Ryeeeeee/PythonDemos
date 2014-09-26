0de28bb45a6c899493e3630cb242c95d80e0834e
Ryeeeeee <spatblan@gmail.com>
2014-09-27 01:18:29 +0800

    commit getAllFiles.py

---
+++ practices/getAllFiles.py
@@ -0,0 +1,19 @@
+# getAllFiles.py
+# get all files(not including directory) in the specified path
+
+import os
+
+def getAllFiles(path):
+    currentFiles = os.listdir(path)
+    allFiles = []
+    for fileName in currentFiles:
+        fullFileName = os.path.join(path, fileName)
+        if os.path.isdir(fullFileName):
+            nextDirs = getAllFiles(fullFileName)
+            allFiles.extend(nextDirs)
+        else:
+            allFiles.append(fullFileName)
+    return allFiles
+
+if __name__ == '__main__':
+    print getAllFiles('.')
